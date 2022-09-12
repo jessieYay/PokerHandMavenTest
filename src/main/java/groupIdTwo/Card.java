7b58b99d2463243a929523b7756426bc14ccc9f3
jessieYay <99616303+jessieYay@users.noreply.github.com>
2022-09-12 17:41:52 +0200

    🔵 should score high card

--- src/main/java/groupIdTwo/Card.java
+++ src/main/java/groupIdTwo/Card.java
@@ -1,7 +1,11 @@
 package groupIdTwo;
 
 public class Card {
+
+    private Rank rank;
+
     public Card(Rank rank, Suit suit) {
 
+        this.rank = rank;
     }
 }
--- src/main/java/groupIdTwo/PokerHand.java
+++ src/main/java/groupIdTwo/PokerHand.java
@@ -3,12 +3,15 @@ package groupIdTwo;
 public class PokerHand {
 
 
+    private Card[] cards;
 
     public PokerHand(Card[] cards) {
 
+        this.cards = cards;
     }
 
     public String getDescription() {
+
         return "high card: Ace";
     }
 }
